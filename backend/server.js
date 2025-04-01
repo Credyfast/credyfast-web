b8d793b5866aac3d7dcaa58dc79ca8051fbc1574
Credyfast <cf.mauricio.mejia@gmail.com>
2025-04-01 14:30:05 -0600

    4.3

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,10 @@ app.use(cors()); // Para permitir peticiones desde el frontend
 
 // Configurar conexión con MySQL en AWS
 const db = mysql.createConnection({
-    host: "tu-host-en-aws",  
-    user: "tu-usuario",  
-    password: "tu-contraseña",  
-    database: "credyfast"
+    host: "credyfastdb.chok4o2koiff.us-east-2.rds.amazonaws.com",  
+    user: "admin",  
+    password: "Credyfast2025.",  
+    database: "credyfastdb"
 });
 
 db.connect(err => {
@@ -38,6 +38,8 @@ app.post("/clientes", (req, res) => {
 });
 
 // Iniciar el servidor en el puerto 3000
-app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000");
+const port = process.env.PORT || 3306; // El puerto lo puedes obtener de la variable de entorno o usar 3000 por defecto
+
+app.listen(port, '0.0.0.0', () => { // Escuchar en todas las interfaces
+    console.log(`Servidor corriendo en http://<IP_PUBLICA>:${port}`);
 });
